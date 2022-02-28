a635ef1757397c2ce376f1636956c24b3b379213
v1rtus2424@gmail.com <v1rtus2424@gmail.com>
2022-02-28 19:20:55 +0700

    Ветка разработки

--- CapacityСalculationUI/AddEditWell.Designer.cs
+++ CapacityСalculationUI/AddEditWell.Designer.cs
@@ -39,7 +39,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(104, 107);
+            this.button2.Location = new System.Drawing.Point(103, 107);
             this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(70, 25);
@@ -62,7 +62,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(33, 107);
+            this.button1.Location = new System.Drawing.Point(32, 107);
             this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(70, 25);
